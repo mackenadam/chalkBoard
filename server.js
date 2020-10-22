bb0fc6eef00676d04c10a953bf434c1cb0de84eb
oli-clive-griffin <o.clivegriffin@gmail.com>
2020-10-22 16:39:32 +1300

    initial commit

--- server.js
+++ server.js
@@ -12,4 +12,21 @@ server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({extended: false}))
 
+//routes
+server.get('/', (req, res) => {
+  res.render('index')
+})
+
+server.get('/compliments', (req, res) => {
+  res.render('compliments')
+})
+
+server.get('/advice', (req, res) => {
+  res.render('advice')
+})
+
+server.get('/vent', (req, res) => {
+  res.render('vent')
+})
+
 module.exports = server
